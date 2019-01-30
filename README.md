9644bafff33ad1d9699193d648be621c9bdeb18b
Mohammed Mosayed <mohammedmosayed@gmail.com>
2019-01-29 19:08:47 -0500

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# React Intro PSET
+
+![prev](pset-prev.png)
+
+
+# Task
+
+- Using the HTML file please create this using React components
+- Try to break down everything into small components
+- Pass in the appropriate `props`
+- Use the `data.json` file to populate the data
+
---
+++ data.js
@@ -0,0 +1,22 @@
+const data = {
+  username: "Mohammed Mosayed",
+  bio: "I am the Web Instructor for Pursuit",
+  posts: [
+    { 
+      image: 'https://source.unsplash.com/1600x900/?nature,water&h=1',
+      caption: 'My trip to the beach'
+    },
+    { 
+      image: 'https://source.unsplash.com/1600x900/?nature,water&h=2',
+      caption: 'My trip to the beach'
+    },
+    { 
+      image: 'https://source.unsplash.com/1600x900/?nature,water&h=3',
+      caption: 'My trip to the beach'
+    },
+    { 
+      image: 'https://source.unsplash.com/1600x900/?nature,water&h=4',
+      caption: 'My trip to the beach'
+    }
+  ]
+}
